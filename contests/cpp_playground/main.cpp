2ebf910a978fd5d57e88184fa9295a269a5dc35b
max <mshchemilkin@gmail.com>
2022-03-21 23:13:11 +0300

    Auto stash before rebase of "origin/main"

--- contests/cpp_playground/main.cpp
+++ contests/cpp_playground/main.cpp
@@ -3,28 +3,7 @@
 
 using namespace std;
 
-vector<string> ans;
-
-void generate (int opened, int left, string tmp) {
-    if (opened == left) {
-        for (int i = 0; i < left; i++) tmp += ')';
-        ans.push_back(tmp);
-        tmp = "";
-        return;
-    }
-    if (opened > left || left <= 0 || opened < 0) {
-        return;
-    }
-    generate(opened + 1, left - 1, tmp + '(');
-    generate(opened - 1, left - 1, tmp + ')');
-}
-
 
 int main(){
-    int n;
-    cin >> n;
-    string s;
-    generate(0, 2*n, s);
-    for (const string& str : ans)
-        cout << str << endl;
+    
 }
