c1d1f46accdac82a1285a38c86df65691635a409
max <mshchemilkin@gmail.com>
2022-05-03 14:15:27 +0300

    some changes

--- contests/cpp_playground/main.cpp
+++ contests/cpp_playground/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
+int factmod (int n, int p) {
+ int res = 1;
+ while (n > 1) {
+  res = (res * ((n/p) % 2 ? p-1 : 1)) % p;
+  for (int i=2; i<=n%p; ++i)
+   res = (res * i) % p;
+  n /= p;
+ }
+ return res % p;
+}
 
 int main(){
-    
+    int m;
+    cin >> m;
+    for (int i=0; i<=m; i++){
+        int n, t;
+        cin >> n >> t;
+        if (t <= n)
+            cout << 0 << endl;
+        else
+            cout << factmod(n, t) << endl;
+    }
 }
